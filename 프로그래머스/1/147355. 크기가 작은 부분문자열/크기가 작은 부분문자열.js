ef909863309619dd8c8b0596d373031e8e646135
sori-sodong <89089430+sori-sodong@users.noreply.github.com>
2023-10-31 20:43:55 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -2,15 +2,8 @@ function solution(t, p) {
     var answer = 0;
     const firstP = p.split('')[0];
     for(let i = 0; i<=t.length-p.length; i++) {
-        
         if(t[i] <= p) {
-            let _t = '';
-            for(let j = 0; j < p.length; j++) {
-                let index = j+i;
-                _t += t[index];
-                
-            }
-            console.log(_t, p, _t<p)
+            const _t = t.slice(i,p.length+i);
             if(_t <= p) answer++;
         }
     }
